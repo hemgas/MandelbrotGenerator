cc4ab1a9532d2db3a9f2e467f202b7b3c7165111
Gaston Hemmerling <hemmgas@gmail.com>
2025-06-10 23:40:12 +0200

    Something

---
+++ MandelbrotGenerator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MandelbrotGenerator
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			var sizeX = 1024; // (Int32) (Image.ActualWidth / 96);
+			var sizeY = 1024; // (Int32) (Image.ActualHeight / 96);
+			var m = MandelbrotImage.Create(
+										pixelWidth: sizeX,
+										pixelHight: sizeY,
+										center: Complex.Zero,
+										zoomFactor: sizeX / 3.0,
+										maxIterations: 16,
+										escapeRadius: 10000
+									);
+			var bitmap = BitmapSource.Create(
+										pixelWidth: sizeX,
+										pixelHeight: sizeY,
+										dpiX: 96,
+										dpiY: 96,
+										pixelFormat: PixelFormats.Gray8,
+										palette: null,
+										pixels: m,
+										stride: 1024
+									);
+			Image.Source = bitmap;
+		}
+
+		private void Image_Loaded(Object sender, RoutedEventArgs e)
+		{
+        }
+    }
+}
---
+++ MandelbrotGenerator/MandelbrotSet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotGenerator
+{
+	internal class MandelbrotSet(Int32 maxIterations, Double escapeRadius)
+	{
+		public Int32 MaxIterations => maxIterations;
+
+		public Double EscapeRadius => escapeRadius;
+
+		public Int32 GetEscapeIterations(Complex c)
+		{
+			var z = Complex.Zero;
+			for (var i = 0; i < maxIterations; ++i) {
+				if (AbsSquare(z) > escapeRadiusSquare) {
+					return i;
+				}
+				z = Square(z) + c;
+			}
+			return maxIterations;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static Double Square(Double value) => value * value;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static Complex Square(Complex value) => value * value;
+
+		private static Double AbsSquare(Complex c) => Square(c.Real) + Square(c.Imaginary);
+
+		private readonly Double escapeRadius = escapeRadius;
+		private readonly Double escapeRadiusSquare = escapeRadius * escapeRadius;
+		private readonly Int32 maxIterations = maxIterations;
+	}
+
+	class MandelbrotImage
+	{
+		public static Byte[] Create(	Int32 pixelWidth,
+										Int32 pixelHight,
+										Complex center,
+										Double zoomFactor,
+										Int32 maxIterations,
+										Double escapeRadius,
+										Double aspectRatio = 1.0)
+		{
+			var realWidth = pixelWidth / zoomFactor;
+			var realMin   = center.Real - realWidth / 2;
+			var realStep  = realWidth / pixelWidth;
+
+			var imaginaryHight = pixelHight / zoomFactor * aspectRatio;
+			var imaginaryMin   = center.Imaginary - imaginaryHight / 2;
+			var imaginaryStep  = imaginaryHight / pixelHight;
+
+			var result = new Byte[pixelWidth * pixelHight];
+
+			var mandelbrotSet = new MandelbrotSet(maxIterations, escapeRadius);
+
+			var index = 0;
+			var real = realMin;
+			for (var x = 0; x < pixelWidth; ++x) {
+				var imaginary = imaginaryMin;
+				for (var y = 0; y < pixelHight; ++y) {
+					result[index++] = (Byte) (mandelbrotSet.GetEscapeIterations(new Complex(real, imaginary)) * Byte.MaxValue / maxIterations);
+					imaginary += imaginaryStep;
+				}
+				real += realStep;
+			}
+
+			return result;
+		}
+
+	}
+}
